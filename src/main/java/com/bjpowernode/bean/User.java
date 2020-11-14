c96234c09e0903818b42c30877df3986c5c76292
YuChenXI <2691210632@qq.com>
2020-11-14 16:12:09 +0800

    新建用户实体类

---
+++ src/main/java/com/bjpowernode/bean/User.java
@@ -0,0 +1,17 @@
+package com.bjpowernode.bean;
+
+/**
+ * @ProjectName: git
+ * @Package: com.bjpowernode.bean
+ * @Description:
+ * @Author: 郭威成
+ * @CreateDate: 2020/11/14 16:10
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+
+public class User {
+    private String username;
+    private String password;
+}
